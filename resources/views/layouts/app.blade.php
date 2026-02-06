2f350a1ee7f69dea4b3dbe61760096d9c04a8b15
Araya05s <akhsanaraya5@gmail.com>
2026-02-06 22:07:41 +0700

    Adding AppController

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <script src="https://unpkg.com/htmx.org@1.9.12"></script>
+
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
--- routes/auth.php
+++ routes/auth.php
@@ -57,3 +57,7 @@ Route::middleware('auth')->group(function () {
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
 });
+
+Route::middleware('auth')->group(function () {
+    Route::get('/app', [AppController::class, 'index'])->name('app');
+});
